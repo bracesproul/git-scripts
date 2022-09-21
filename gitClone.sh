d4f11a1a0c1856c493b106d5e929d9f0263cbf09
Brace Sproul <braceasproul@gmail.com>
2022-09-21 13:07:04 -0700

    added custom branch name option

--- gitClone.sh
+++ gitClone.sh
@@ -20,9 +20,11 @@ if [ $full_url_y_n = "y" ]
         IFS=/ read -r str1 str2 str3 full_url_username full_url_repo <<< $full_git_url
         IFS=. read -r full_url_repo_name __dot_git__ <<< $full_url_repo
 
+
+
         printf "${BGREEN}Git URL: ${full_git_url}${RESET}\n"
         printf "\n${BLUE}Select branch to use${RESET}\n"
-        select newBranch in dev_branch main_branch quit; do
+        select newBranch in dev_branch main_branch custom quit; do
             case $newBranch in
                 dev_branch)
                     printf "${BGREEN}Selected ${UCyan}${dev_branch}${RESET} ${BGREEN}branch\n"
@@ -37,8 +39,7 @@ if [ $full_url_y_n = "y" ]
                     printf "${BGREEN}Installing dependencies...${RESET}\n"
                     npm install
                     printf "\n${BGREEN}Navigate to new repo by running ${UCyan}cd ./${full_url_repo_name}${RESET}"
-                    break 2
-                    ;;
+                    break 2;;
                 main_branch)
                     printf "${BGREEN}Selected ${UCyan}${main_branch}${RESET} ${BGREEN}branch\n"
                     printf "Cloning into repo: ${UCyan}${full_git_url}${RESET}\n"
@@ -52,8 +53,22 @@ if [ $full_url_y_n = "y" ]
                     printf "${BGREEN}Installing dependencies...${RESET}\n"
                     npm install
                     printf "\n${BGREEN}Navigate to new repo by running ${UCyan}cd ./${full_url_repo_name}${RESET}"
-                    break 2
-                    ;;
+                    break 2;;
+                custom)
+                    printf "${BLUE}Enter branch name: ${RESET}"
+                    read branch_name
+                    printf "Cloning into repo: ${UCyan}${full_git_url}${RESET}\n"
+                    git clone $full_git_url
+                    printf "${BGREEN}changing directory => ${UCyan}${full_url_repo_name}${RESET}\n"
+                    cd $full_url_repo_name
+                    printf "${BGREEN}Creating branch: ${UCyan}${branch_name}${RESET}\n" 
+                    git branch $branch_name
+                    git checkout $branch_name
+                    printf "\n${BGREEN}Finished cloning into ${UCyan}${full_url_username}/${full_url_repo_name}${RESET} ${BGREEN}and selecting branch ${UCyan}${branch_name}${RESET}\n"
+                    printf "${BGREEN}Installing dependencies...${RESET}\n"
+                    npm install
+                    printf "\n${BGREEN}Navigate to new repo by running ${UCyan}cd ./${full_url_repo_name}${RESET}"
+                    break 2;;
                 quit)
                     printf "${BRED}Exiting script.${RESET}"
                     break 2;;
@@ -79,7 +94,7 @@ printf "${BLUE}Github repo name: ${RESET}"
 read git_repo_name
 git_url=$git_username/$git_repo_name
 
-select branch in dev_branch main_branch quit; do
+select branch in dev_branch main_branch custom quit; do
     case $branch in
         dev_branch)
             printf "${BGREEN}Selected ${UCyan}${dev_branch}${RESET} ${BGREEN}branch\n"
@@ -94,8 +109,7 @@ select branch in dev_branch main_branch quit; do
             printf "${BGREEN}Installing dependencies...${RESET}\n"
             npm install
             printf "\n${BGREEN}Navigate to new repo by running ${UCyan}cd ./${git_repo_name}${RESET}"
-            break 2
-            ;;
+            break 2;;
         main_branch)
             printf "${BGREEN}Selected ${UCyan}${main_branch}${RESET} ${BGREEN}branch\n"
             printf "Cloning into repo: ${UCyan}${git_username}/${git_repo_name}${RESET}\n"
@@ -109,8 +123,22 @@ select branch in dev_branch main_branch quit; do
             printf "${BGREEN}Installing dependencies...${RESET}\n"
             npm install
             printf "\n${BGREEN}Navigate to new repo by running ${UCyan}cd ./${git_repo_name}${RESET}"
-            break 2
-            ;;
+            break 2;;
+        custom)
+            printf "${BLUE}Enter branch name: ${RESET}"
+            read branch_name
+            printf "Cloning into repo: ${UCyan}${git_username}/${git_repo_name}${RESET}\n"
+            git clone https://github.com/$git_username/$git_repo_name.git
+            printf "${BGREEN}changing directory => ${UCyan}${git_repo_name}${RESET}\n" 
+            cd $git_repo_name
+            printf "${BGREEN}Creating branch: ${UCyan}${branch_name}${RESET}\n" 
+            git branch $branch_name
+            git checkout $branch_name
+            printf "\n${BGREEN}Finished cloning into ${UCyan}${git_url}${RESET} ${BGREEN}and selecting branch ${UCyan}${branch_name}${RESET}\n"
+            printf "${BGREEN}Installing dependencies...${RESET}\n"
+            npm install
+            printf "\n${BGREEN}Navigate to new repo by running ${UCyan}cd ./${git_repo_name}${RESET}"
+            break 2;;
         quit)
             echo "Exiting script"
             break 2;;
